b30a4694bf9169d6504da33964c7e0c648e9e254
AdamM <44504469+Ahmonds@users.noreply.github.com>
2021-07-20 13:30:56 -0400

    Update SMA.ino

--- SMA.ino
+++ SMA.ino
@@ -4,13 +4,13 @@ class SMA {
   byte Pos = 0;
   public :
   float average (float reading) {
-    total -= readings[Pos];       //subtract old reading from total
-    readings[Pos] = reading;      //place new reading into the array
-    total += readings[Pos];       //add the new reading to the total
+    total -= readings[Pos];         //subtract old reading from total
+    readings[Pos] = reading;        //place new reading into the array
+    total += readings[Pos];         //add the new reading to the total
     (Pos < n-1 ? Pos++ : Pos = 0);  //Incrament Pos until the end is reached
-    return total / n;             //take the current average
+    return total / n;               //take the current average
   }
-  SMA () {
+  SMA () {                          //Constrctor to clear/set reading array
     for (byte t = 0; t < n; t++) {
     readings[t] = 0;
     }
